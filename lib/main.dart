5dd241608c9589e4023b9ef4c365ecc09c5a196a
Denis <denisdiogo18@hotmail.com>
2024-11-14 08:30:11 -0300

    Primary commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:project_facebook/screens/home.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Faceook",
+    home: Home(),
+  ));
+}
